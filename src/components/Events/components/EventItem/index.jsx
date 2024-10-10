9e3a04242c555cc246f66aa00a2078470e21a1b8
David Jaimes <david.caracas@hotmail.com>
2024-10-10 01:35:48 -0400

    Lisat eventos

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,6 +1,10 @@
-const EventItem = () => {
+const EventItem = ({ info, name, image}) => {
     return (
-        <div>Event Item</div>
+        <div>
+            <img src={image} alt={name} width={200} height={200} />
+            <h4>{name}</h4>
+            <p>{info}</p>
+        </div>
     );
 };
 
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,9 +1,23 @@
-import EventItem from './components/EventItem'
+import { useState } from 'react'
+import eventsJSON from '../../data/events.json';
+import EventItem from './components/EventItem';
 
 const Events = () => {
+    const [data] = useState(eventsJSON);   
+    const { _embedded: { events }} = data; 
     return (
         <div>
             Eventos
+            {
+                events.map((eventItem) => (
+                    <EventItem 
+                        key={`event-item-${eventItem.id}`} 
+                        name={eventItem.name}
+                        info={eventItem.info}
+                        image={eventItem.images[0].url}
+                    />
+                ))
+            }
         </div>
     );
 };
